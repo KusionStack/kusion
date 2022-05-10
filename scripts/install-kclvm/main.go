3740b50b0fd5e816811109ff943717b967cfe866
chai2010 <chaishushan@gmail.com>
2022-05-10 20:43:05 +0800

    refine build script

---
+++ scripts/install-kclvm/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"kusionstack.io/kclvm-go/scripts"
+)
+
+var (
+	flagTriple = flag.String("triple", "", "set kclvm triple")
+	flagOutdir = flag.String("outdor", "", "set output dir")
+
+	flagMirrors = flag.String("mirrors", "", "set mirror url")
+)
+
+func main() {
+	flag.Parse()
+	if *flagTriple == "" || *flagOutdir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	scripts.DefaultKclvmTriple = *flagTriple
+	if s := *flagMirrors; s != "" {
+		for _, s := range strings.Split(s, ",") {
+			s := strings.TrimSpace(s)
+			if s != "" {
+				scripts.KclvmDownloadUrlBase_mirrors = append(scripts.KclvmDownloadUrlBase_mirrors, s)
+			}
+		}
+	}
+
+	err := scripts.SetupKclvm(*flagOutdir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
