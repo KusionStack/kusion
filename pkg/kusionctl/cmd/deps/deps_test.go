c28949c5fd62785ea565e4a7b890b584260fc562
amyxia <xiarui1994@gmail.com>
2022-07-18 15:44:12 +0800

    Feature: support deleted files in kusion deps cmd
    * BugFix: deps subcommand supports deleted files in change list
    
    * Update: kusion deps reduce time cost
    
    * Update: kusion deps add unit and bench tests
    
    * Update go.mod
    
    * add more tests
    
    * fix lint problem
    
    * update kclvm-go version to v0.4.3-alpha.1
    
    * Update deps/options.go: comments/redundant funcs
    
    * revert removing toSlice function

---
+++ pkg/kusionctl/cmd/deps/deps_test.go
@@ -0,0 +1,292 @@
+package deps
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"kusionstack.io/kusion/pkg/projectstack"
+)
+
+var workDir string
+
+func init() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		os.Exit(1)
+	}
+	workDir = filepath.Join(cwd, "testdata")
+}
+
+func TestNewCmdDeps(t *testing.T) {
+	cmd := NewCmdDeps()
+	if err := cmd.Execute(); err == nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDepsOptions_Validate(t *testing.T) {
+	tCases := []struct {
+		name    string
+		only    string
+		direct  string
+		workDir string
+		focus   []string
+		ignore  []string
+		errMsg  string
+	}{
+
+		{
+			name:   "invalid output filter",
+			errMsg: "invalid output downstream type. supported types: project, stack",
+		},
+		{
+			name:   "invalid direct",
+			only:   "project",
+			errMsg: "invalid output direction of the dependency inspection. supported directions: up, down",
+		},
+		{
+			name:   "invalid workdir",
+			only:   "project",
+			direct: "up",
+			errMsg: "invalid work dir: stat : no such file or directory",
+		},
+		{
+			name:    "invalid focus",
+			only:    "project",
+			direct:  "up",
+			workDir: workDir,
+			errMsg:  "invalid focus paths. cannot be empty",
+		},
+		{
+			name:    "invalid ignore",
+			only:    "project",
+			direct:  "up",
+			workDir: workDir,
+			focus:   []string{"file.k"},
+			ignore:  []string{"invalid_path.k"},
+			errMsg:  fmt.Sprintf("invalid ignore path. need to be valid relative path from the workdir: stat %s: no such file or directory", filepath.Join(workDir, "invalid_path.k")),
+		},
+		{
+			name:    "valid",
+			only:    "project",
+			direct:  "up",
+			workDir: workDir,
+			focus:   []string{"file.k"},
+			errMsg:  "",
+		},
+	}
+	for _, tc := range tCases {
+		t.Run(tc.name, func(t *testing.T) {
+			opt := DepsOptions{
+				workDir: tc.workDir,
+				Direct:  tc.direct,
+				Only:    tc.only,
+				Focus:   tc.focus,
+				Ignore:  tc.ignore,
+			}
+			err := opt.Validate()
+			if err != nil && err.Error() != tc.errMsg {
+				t.Fatalf("wrong validate errMsg, actual: %s, expect: %s", err.Error(), tc.errMsg)
+			}
+			if err == nil && tc.errMsg != "" {
+				t.Fatalf("wrong validate result: actual: validate success, expect err: %s", tc.errMsg)
+			}
+		})
+	}
+}
+
+func TestDepsOptions_Complete(t *testing.T) {
+	tCases := []struct {
+		name      string
+		args      []string
+		completed string
+	}{
+		{
+			name:      "omit workdir in args",
+			completed: filepath.Dir(workDir),
+		},
+		{
+			name:      "specify workdir in args",
+			args:      []string{"workdir path"},
+			completed: "workdir path",
+		},
+	}
+	for _, tc := range tCases {
+		t.Run(tc.name, func(t *testing.T) {
+			opt := NewDepsOptions()
+			opt.Complete(tc.args)
+			if opt.workDir != tc.completed {
+				t.Fatalf("wrong completed workdir, actual: %s, expect: %s", opt.workDir, tc.completed)
+			}
+		})
+	}
+}
+
+func TestDepsOptions_Run(t *testing.T) {
+	tCases := []struct {
+		workDir string
+		direct  string
+		only    string
+	}{
+		{
+			workDir: workDir,
+			direct:  "up",
+		},
+		{
+			workDir: workDir,
+			direct:  "down",
+			only:    "project",
+		},
+	}
+	for _, tc := range tCases {
+		t.Run(tc.direct, func(t *testing.T) {
+			opt := DepsOptions{
+				workDir: tc.workDir,
+				Direct:  tc.direct,
+				Only:    tc.only,
+			}
+			if err := opt.Run(); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestDepsOptions_Run2(t *testing.T) {
+	opt := DepsOptions{
+		workDir: workDir,
+		Direct:  "up",
+		Focus: []string{
+			"appops/projectC/dev/main.k",
+		},
+	}
+	err := opt.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+var downstreamTestCases = []struct {
+	name               string
+	focus              []string
+	downStreamProjects []string
+	ignore             []string
+}{
+	{
+		name: "change base and entrance files",
+		focus: []string{
+			"base/frontend/container/container_port.k",
+			"appops/projectA/base/base.k",
+			"appops/projectA/dev/main.k",
+			"appops/projectA/dev/datafile.sql",
+		},
+		downStreamProjects: []string{
+			"appops/projectA",
+			"appops/projectB",
+			"appops/projectC",
+		},
+	},
+	{
+		name:  "change common base: container port",
+		focus: []string{"base/frontend/container/container_port.k"},
+		downStreamProjects: []string{
+			"appops/projectA",
+			"appops/projectB",
+			"appops/projectC",
+		},
+	},
+	{
+		name:  "change server render base",
+		focus: []string{"base/render/server/server_render.k"},
+		downStreamProjects: []string{
+			"appops/projectA",
+			"appops/projectC",
+		},
+	},
+	{
+		name:  "change job render base",
+		focus: []string{"base/render/job/job_render.k"},
+		downStreamProjects: []string{
+			"appops/projectB",
+		},
+	},
+	{
+		name: "ignore job render base",
+		focus: []string{
+			"base/render/server/server_render.k",
+			"base/render/job/job_render.k",
+		},
+		ignore: []string{
+			"base/render/job/job_render.k",
+		},
+		downStreamProjects: []string{
+			"appops/projectA",
+			"appops/projectC",
+		},
+	},
+	{
+		name: "only change entrance files",
+		focus: []string{
+			"appops/projectA/base/base.k",
+			"appops/projectA/dev/main.k",
+			"appops/projectB/dev/main.k",
+		},
+		downStreamProjects: []string{
+			"appops/projectA",
+			"appops/projectB",
+		},
+	},
+	{
+		name: "only change data files",
+		focus: []string{
+			"appops/projectA/dev/datafile.sql",
+		},
+		downStreamProjects: []string{
+			"appops/projectA",
+		},
+	},
+	{
+		name: "delete files",
+		focus: []string{
+			"appops/projectC/dev/non_exist.sql",
+			"base/frontend/non_exist/non_exist.k",
+		},
+		downStreamProjects: []string{
+			"appops/projectC",
+		},
+	},
+}
+
+func TestFindDownStreams(t *testing.T) {
+	projects, err := projectstack.FindAllProjectsFrom(workDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, tc := range downstreamTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := findDownStreams(workDir, projects, toSet(tc.focus), toSet(tc.ignore), true)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.ElementsMatch(t, result.toSlice(), tc.downStreamProjects, "test result mismatch")
+		})
+	}
+}
+
+func BenchmarkDownStream(b *testing.B) {
+	tc := downstreamTestCases[0]
+	for i := 0; i < b.N; i++ {
+		projects, err := projectstack.FindAllProjectsFrom(workDir)
+		if err != nil {
+			b.Fatal(err)
+		}
+		result, err := findDownStreams(workDir, projects, toSet(tc.focus), toSet(tc.ignore), true)
+		if err != nil {
+			b.Fatal(err)
+		}
+		assert.ElementsMatch(b, result.toSlice(), tc.downStreamProjects, "test result mismatch")
+	}
+}
--- pkg/kusionctl/cmd/deps/options.go
+++ pkg/kusionctl/cmd/deps/options.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	kcl "kusionstack.io/kclvm-go"
 	"kusionstack.io/kclvm-go/pkg/tools/list"
@@ -16,6 +18,40 @@ type DepsOptions struct {
 	Focus   []string
 	Only    string
 	Ignore  []string
+}
+
+// stringSet is a simple string set implementation by map
+type stringSet map[string]bool
+
+// emptyStringSet creates a string set with an empty value list
+func emptyStringSet() stringSet {
+	return make(stringSet)
+}
+
+// add a string value to the stringSet s
+func (s stringSet) add(value string) {
+	s[value] = true
+}
+
+// remove a string value from the stringSet s
+func (s stringSet) remove(value string) {
+	delete(s, value)
+}
+
+// contains checks if the stringSet s contains certain string value
+func (s stringSet) contains(value string) bool {
+	_, ok := s[value]
+	return ok
+}
+
+// toSlice generates a string slice containing all the string values in the stringSet s
+func (s stringSet) toSlice() []string {
+	result := make([]string, 0, len(s))
+	for value := range s {
+		result = append(result, value)
+	}
+	sort.Strings(result)
+	return result
 }
 
 func NewDepsOptions() *DepsOptions {
@@ -49,12 +85,6 @@ func (o *DepsOptions) Validate() error {
 		return fmt.Errorf("invalid focus paths. cannot be empty")
 	}
 
-	for _, focus := range o.Focus {
-		if _, err := os.Stat(filepath.Join(o.workDir, focus)); err != nil {
-			return fmt.Errorf("invalid focus path. need to be valid relative path from the workdir: %s", err)
-		}
-	}
-
 	for _, ignore := range o.Ignore {
 		if _, err := os.Stat(filepath.Join(o.workDir, ignore)); err != nil {
 			return fmt.Errorf("invalid ignore path. need to be valid relative path from the workdir: %s", err)
@@ -63,120 +93,176 @@ func (o *DepsOptions) Validate() error {
 	return nil
 }
 
-func (o *DepsOptions) Run() error {
+func (o *DepsOptions) Run() (err error) {
 	workDir, err := filepath.Abs(o.workDir)
 	if err != nil {
-		return err
+		return
 	}
 	o.workDir = workDir
 	switch o.Direct {
+	// List upstream files of the focus files
 	case "up":
-		depsFiles, err := list.ListUpStreamFiles(o.workDir, &list.DepOption{Files: o.Focus})
+		upstreamFiles, err := list.ListUpStreamFiles(o.workDir, &list.DepOptions{Files: o.Focus})
 		if err != nil {
 			return err
 		}
-		for _, v := range depsFiles {
-			fmt.Println(v)
+		for _, f := range upstreamFiles {
+			fmt.Println(f)
 		}
 		return nil
+	// List downstream files of the focus files
 	case "down":
-		projects, err := projectstack.FindAllProjectsFrom(o.workDir)
-		if err != nil {
-			return err
-		}
-		file2StackMap := map[string][]string{}
-		file2ProjMap := map[string][]string{}
-		entranceFiles := []string{}
-		ignoreMap := map[string]bool{}
-		for _, ignore := range o.Ignore {
-			ignoreMap[ignore] = true
-		}
-		for _, project := range projects {
-			relProjPath, err := filepath.Rel(o.workDir, project.GetPath())
-			if err != nil {
-				return err
-			}
-			for _, stack := range project.Stacks {
-				relStackPath, err := filepath.Rel(o.workDir, stack.GetPath())
-				if err != nil {
-					return err
-				}
-				opt := kcl.WithSettings(filepath.Join(stack.GetPath(), projectstack.KclFile))
-				for _, entranceFile := range opt.KFilenameList {
-					relPath, err := filepath.Rel(o.workDir, entranceFile)
-					if err != nil {
-						return err
-					}
-					if _, ok := ignoreMap[relPath]; ok {
-						continue
-					}
-					file2StackMap[relPath] = append(file2StackMap[relPath], relStackPath)
-					file2ProjMap[relPath] = append(file2ProjMap[relPath], relProjPath)
-					entranceFiles = append(entranceFiles, relPath)
-				}
-				sFiles, err := listFiles(stack.GetPath(), true)
-				if err != nil {
-					return err
-				}
-				for _, file := range sFiles {
-					rel, _ := filepath.Rel(o.workDir, file)
-					if _, ok := file2StackMap[rel]; !ok {
-						file2StackMap[rel] = append(file2StackMap[rel], relStackPath)
-						file2ProjMap[rel] = append(file2ProjMap[rel], relProjPath)
-					}
-				}
-			}
-		}
-		affectedFiles, err := kcl.ListDownStreamFiles(o.workDir, &list.DepOption{
-			Files:        entranceFiles,
-			ChangedPaths: o.Focus,
-		})
-		if err != nil {
-			return err
-		}
-		var fileMap map[string][]string
+		// 1. Check which are wanted: downstream projects or downstream stacks
+		var projectOnly bool
 		switch o.Only {
 		case "project":
-			fileMap = file2ProjMap
+			projectOnly = true
 		case "stack":
-			fileMap = file2StackMap
+			projectOnly = false
 		default:
 			return fmt.Errorf("invalid output downstream type. supported types: project, stack")
 		}
-		affected := map[string]bool{}
-		for _, affect := range affectedFiles {
-			// filter
-			if stacks, ok := fileMap[affect]; ok {
-				for _, stack := range stacks {
-					affected[stack] = true
-				}
-			}
+
+		// 2. Index the paths that should be ignored and the paths that need to list downstream stacks/projects on.
+		shouldIgnore := toSet(o.Ignore)
+		focusPaths := toSet(o.Focus)
+
+		// 3. Find all the projects under the workdir
+		var projects []*projectstack.Project
+		if projects, err = projectstack.FindAllProjectsFrom(workDir); err != nil {
+			return
 		}
-		for name := range affected {
+
+		// 4. Filter the downstream stacks/projects of the focusPaths
+		downstreams, err := findDownStreams(o.workDir, projects, focusPaths, shouldIgnore, projectOnly)
+		if err != nil {
+			return err
+		}
+
+		// 5. Output the result
+		for name := range downstreams {
 			fmt.Println(name)
 		}
 		return nil
 	default:
-		return fmt.Errorf("unsupport diretion")
+		return fmt.Errorf("unsupport diretion of deps")
 	}
 }
 
-func listFiles(root string, resursive bool) ([]string, error) {
-	entries, err := os.ReadDir(root)
-	if err != nil {
-		return nil, err
-	}
-	files := []string{}
-	for _, file := range entries {
-		if !file.IsDir() {
-			files = append(files, filepath.Join(root, file.Name()))
-		} else if resursive {
-			subFiles, err := listFiles(filepath.Join(root, file.Name()), resursive)
-			if err != nil {
-				return nil, err
+// findDownStreams finds all the downstream projects/stacks of the focusPaths in the given workDir.
+// DownStreams can be downstream projects or downstream stacks, up to the projectOnly parameter.
+// By downstream stacks, it means that either the entrances files of those stacks are downstream files of the focus paths or
+// the files under those stacks' directories appear in the focus paths list.
+// By downstream projects, it means that each of those projects contains one or more downstream stacks of the focus paths.
+//
+// Parameters
+//
+// The workDir should be a valid absolute path of the root path of the KCL program directory.
+// The focusPaths and shouldIgnore should be valid relative file paths under the workDir.
+// The value of the projectOnly decides whether the downstream projects(projectOnly is true) or stacks(projectOnly is false) will be filtered.
+//
+// Usage Caution
+//
+// This is a very time-consuming function based on the FindAllProjectsFrom API of kusion and the ListDownStreamFiles API of kcl.
+// Do not call this function with high frequency and please ensure at least 10 seconds interval when calling.
+func findDownStreams(workDir string, projects []*projectstack.Project, focusPaths, shouldIgnore stringSet, projectOnly bool) (downStreams stringSet, err error) {
+	entrances := emptyStringSet()               // all the entrance files in the work directory
+	entranceIndex := make(map[string]stringSet) // entrance file index to the corresponding projects/stacks
+	downStreams = emptyStringSet()              // might be downstream projects or downstream stacks, according to the projectOnly option
+
+	// 1. For each project/stack, check if there are files changed in it or collect all the entrance files in them
+	// To list downstream stacks/projects, wee need to go through all the entrance files under each project/stack,
+	// then filter out the ones that are downstream of the focus files
+	for _, project := range projects {
+		projectRel, _ := filepath.Rel(workDir, project.GetPath())
+		for _, stack := range project.Stacks {
+			// 1.1 Get the relative path of project/stack
+			stackRel, _ := filepath.Rel(workDir, stack.GetPath())
+			var stackProjectPath string
+			if projectOnly {
+				stackProjectPath = projectRel
+			} else {
+				stackProjectPath = stackRel
 			}
-			files = append(files, subFiles...)
+			// 1.2 In order to save time, cut off the focus paths which are exactly under some stacks/projects directory,
+			// so that:
+			// a. The corresponding stacks/projects can be directly marked as downstream
+			// b. Those focus paths will be skipped to call the ListDownStreamFiles API
+			isDownstream := false
+			// Iterate all the focus files and check if there appear some files under that stack, and delete those files from the focus paths
+			for f := range focusPaths {
+				if strings.HasPrefix(f, stackRel) {
+					// Skip those focus paths that are under the stack directory
+					focusPaths.remove(f)
+					if !shouldIgnore.contains(f) {
+						isDownstream = true
+					}
+				}
+			}
+			if isDownstream {
+				// Mark the stack/project as downstream
+				downStreams.add(stackProjectPath)
+				continue
+			}
+			// 1.3 Collect and index all the entrance files of the stack by loading the settings file
+			settingsPath := filepath.Join(stack.GetPath(), projectstack.KclFile)
+			opt := kcl.WithSettings(settingsPath)
+			if opt.Err != nil {
+				// The stack settings is invalid
+				err = fmt.Errorf("invalid settings file(%s), %v", settingsPath, err)
+				return
+			}
+			var entranceRels []string
+			for _, entranceFile := range opt.KFilenameList {
+				entranceRel, _ := filepath.Rel(workDir, entranceFile)
+				if shouldIgnore.contains(entranceRel) {
+					// Skip recording entrance files that should be ignored
+					continue
+				}
+				entranceRels = append(entranceRels, entranceRel)
+				if focusPaths.contains(entranceRel) {
+					// As long as one entrance file appears in the focus paths is enough to mark the stack/project as downstream
+					isDownstream = true
+				}
+			}
+			if isDownstream {
+				downStreams.add(stackProjectPath)
+				continue
+			}
+			for _, entranceRel := range entranceRels {
+				entrances.add(entranceRel)
+				if _, ok := entranceIndex[entranceRel]; !ok {
+					entranceIndex[entranceRel] = emptyStringSet()
+				}
+				entranceIndex[entranceRel].add(stackProjectPath)
+			}
 		}
 	}
-	return files, nil
+
+	// 2. Call the ListDownStreamFiles API
+	downstreamFiles, err := kcl.ListDownStreamFiles(workDir, &list.DepOptions{
+		Files:     entrances.toSlice(),
+		UpStreams: focusPaths.toSlice(),
+	})
+	if err != nil {
+		return
+	}
+
+	// 3. For each downstream file, check if it belongs to some stacks/projects, and mark those stacks/projects as downstream
+	for _, file := range downstreamFiles {
+		if stacksOrProjs, ok := entranceIndex[file]; ok {
+			for v := range stacksOrProjs {
+				downStreams.add(v)
+			}
+		}
+	}
+	return
+}
+
+func toSet(list []string) stringSet {
+	result := emptyStringSet()
+	for _, item := range list {
+		result.add(item)
+	}
+	return result
 }
