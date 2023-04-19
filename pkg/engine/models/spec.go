eff201103e4454636064d8e87b9ddb27acccc377
Dayuan <daniellee0929@gmail.com>
2023-04-19 14:47:22 +0800

    delete redundant codes

--- pkg/engine/models/spec.go
+++ pkg/engine/models/spec.go
@@ -4,16 +4,3 @@ package models
 type Spec struct {
 	Resources Resources `json:"resources" yaml:"resources"`
 }
-
-// fixme: get `cluster` from compile arguments
-
-// ParseCluster try to parse Cluster from resource extensions.
-// All resources in one compile MUST have the same Cluster and this constraint will be guaranteed by KCL compile logic
-func (s *Spec) ParseCluster() string {
-	resources := s.Resources
-	var cluster string
-	if len(resources) != 0 && resources[0].Extensions != nil && resources[0].Extensions["Cluster"] != nil {
-		cluster = resources[0].Extensions["Cluster"].(string)
-	}
-	return cluster
-}
