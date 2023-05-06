84e16bf2de77e92d67cb0fcebb38c68d2aa0f960
healthjyk <healtjyk@163.com>
2023-05-06 15:30:07 +0800

    add templates as sub cmd of init, used to list templates, support json format

--- pkg/cmd/init/init.go
+++ pkg/cmd/init/init.go
@@ -12,7 +12,7 @@ var (
 	initShort = `Initialize KCL file structure and base codes for a new project`
 
 	initLong = `
-		kusion init command helps you to generate an scaffolding KCL project.
+		kusion init command helps you to generate a scaffolding KCL project.
 		Try "kusion init" to simply get a demo project.`
 
 	initExample = `
@@ -27,6 +27,19 @@ var (
 
 		# Initialize a new KCL project from local directory
 		kusion init /path/to/templates`
+
+	templatesShort = `List Templates used to initialize a new project`
+
+	templatesLong = `
+		kusion init templates command helps you get the templates which are used
+    to generate a scaffolding KCL project.`
+
+	templatesExample = `
+		# Get name and description of internal templates
+		kusion init templates
+
+		# Get templates from specific templates location
+		kusion init templates https://github.com/<user>/<repo> --online=true`
 )
 
 func NewCmdInit() *cobra.Command {
@@ -52,7 +65,7 @@ func NewCmdInit() *cobra.Command {
 	cmd.Flags().BoolVar(
 		&o.Force, "force", false,
 		i18n.T("Forces content to be generated even if it would change existing files"))
-	cmd.Flags().BoolVar(
+	cmd.PersistentFlags().BoolVar(
 		&o.Online, "online", false,
 		i18n.T("Use locally cached templates without making any network requests"))
 	cmd.Flags().BoolVar(
@@ -61,5 +74,36 @@ func NewCmdInit() *cobra.Command {
 	cmd.Flags().StringVar(
 		&o.CustomParamsJSON, "custom-params", "",
 		i18n.T("Custom params in JSON string; if not empty, kusion will skip prompt process and use it as template default value"))
+
+	templatesCmd := newCmdTemplates()
+	cmd.AddCommand(templatesCmd)
+	return cmd
+}
+
+func newCmdTemplates() *cobra.Command {
+	o := NewTemplatesOptions()
+	cmd := &cobra.Command{
+		Use:                   "templates",
+		Short:                 i18n.T(templatesShort),
+		Long:                  templates.LongDesc(i18n.T(templatesLong)),
+		Example:               templates.Examples(i18n.T(templatesExample)),
+		DisableFlagsInUseLine: true,
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			defer util.RecoverErr(&err)
+			online, err := cmd.InheritedFlags().GetBool("online")
+			if err != nil {
+				return err
+			}
+			util.CheckErr(o.Complete(args, online))
+			util.CheckErr(o.Validate())
+			util.CheckErr(o.Run())
+			return
+		},
+	}
+
+	cmd.Flags().StringVarP(
+		&o.Output, "output", "o", "",
+		i18n.T("The output format, only support json if specified; if not specified, print template name and description"))
+
 	return cmd
 }
