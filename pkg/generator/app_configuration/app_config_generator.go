480dcf39c324bbbf8d305b6202ff7794b6ecc580
elliotxx <951376975@qq.com>
2023-07-31 17:21:18 +0800

    feat: add app configuration model in golang

--- pkg/generator/app_configuration/app_config_generator.go
+++ pkg/generator/app_configuration/app_config_generator.go
@@ -9,39 +9,12 @@ import (
 
 	"kusionstack.io/kusion/pkg/generator"
 	"kusionstack.io/kusion/pkg/models"
+	"kusionstack.io/kusion/pkg/models/appconfiguration"
 	"kusionstack.io/kusion/pkg/projectstack"
 )
 
-// AppConfiguration is a developer-centric definition that describes how to run an Application.
-// This application model builds upon a decade of experience at AntGroup running super large scale
-// internal developer platform, combined with best-of-breed ideas and practices from the community.
-//
-// Example
-//
-//	components:
-//	  proxy:
-//	    containers:
-//	      nginx:
-//	        image: nginx:v1
-//	        command:
-//	        - /bin/sh
-//	        - -c
-//	        - echo hi
-//	        args:
-//	        - /bin/sh
-//	        - -c
-//	        - echo hi
-//	        env:
-//	          env1: VALUE
-//	          env2: secret://sec-name/key
-//	        workingDir: /tmp
-//	    replicas: 2
-type AppConfiguration struct {
-	Components map[string]any
-}
-
 type AppConfigurationGenerator struct {
-	AppConfiguration *AppConfiguration
+	*appconfiguration.AppConfiguration
 }
 
 func (acg *AppConfigurationGenerator) GenerateSpec(
