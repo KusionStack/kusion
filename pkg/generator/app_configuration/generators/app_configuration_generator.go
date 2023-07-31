c4cd1b07106f4b8e9299e85defe1eec4dab68e41
elliotxx <951376975@qq.com>
2023-07-31 21:00:40 +0800

    refactor: refine generator to make it more extensible

---
+++ pkg/generator/app_configuration/generators/app_configuration_generator.go
@@ -0,0 +1,45 @@
+package generators
+
+import (
+	"fmt"
+
+	"kusionstack.io/kusion/pkg/models"
+	"kusionstack.io/kusion/pkg/models/appconfiguration"
+)
+
+type appConfigurationGenerator struct {
+	projectName string
+	ac          *appconfiguration.AppConfiguration
+}
+
+func NewAppConfigurationGenerator(projectName string, ac *appconfiguration.AppConfiguration) (Generator, error) {
+	if len(projectName) == 0 {
+		return nil, fmt.Errorf("project name must not be empty")
+	}
+
+	if ac == nil {
+		return nil, fmt.Errorf("can not find app configuration when generating the Spec")
+	}
+
+	return &appConfigurationGenerator{
+		projectName: projectName,
+		ac:          ac,
+	}, nil
+}
+
+func (g *appConfigurationGenerator) Generate(spec *models.Spec) error {
+	if spec.Resources == nil {
+		spec.Resources = make(models.Resources, 0)
+	}
+
+	gfs := []NewGeneratorFunc{
+		NewNamespaceGeneratorFunc(g.projectName),
+		NewComponentsGeneratorFunc(g.projectName, g.ac.Components),
+	}
+
+	if err := callGenerators(spec, gfs...); err != nil {
+		return err
+	}
+
+	return nil
+}
