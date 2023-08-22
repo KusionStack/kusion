845f28c4fb29a5ee925157a1365aef78124dba0c
elliotxx <951376975@qq.com>
2023-08-22 20:37:10 +0800

    style: complete comment for ordered resource generator

--- pkg/generator/appconfiguration/generator/ordered_resources_generator.go
+++ pkg/generator/appconfiguration/generator/ordered_resources_generator.go
@@ -7,6 +7,7 @@ import (
 	"kusionstack.io/kusion/pkg/models"
 )
 
+// defaultOrderedKinds provides the default order of kubernetes resource kinds.
 var defaultOrderedKinds = []string{
 	"Namespace",
 	"ResourceQuota",
@@ -34,10 +35,12 @@ var defaultOrderedKinds = []string{
 	"ValidatingWebhookConfiguration",
 }
 
+// orderedResourcesGenerator is a generator that inject the dependsOn of resources in a specified order.
 type orderedResourcesGenerator struct {
 	orderedKinds []string
 }
 
+// NewOrderedResourcesGenerator returns a new instance of orderedResourcesGenerator.
 func NewOrderedResourcesGenerator(orderedKindsList ...[]string) (appconfiguration.Generator, error) {
 	orderedKinds := defaultOrderedKinds
 	if len(orderedKindsList) > 0 && len(orderedKindsList[0]) > 0 {
@@ -48,38 +51,38 @@ func NewOrderedResourcesGenerator(orderedKindsList ...[]string) (appconfiguratio
 	}, nil
 }
 
+// NewOrderedResourcesGeneratorFunc returns a function that creates a new orderedResourcesGenerator.
 func NewOrderedResourcesGeneratorFunc(orderedKindsList ...[]string) appconfiguration.NewGeneratorFunc {
 	return func() (appconfiguration.Generator, error) {
 		return NewOrderedResourcesGenerator(orderedKindsList...)
 	}
 }
 
+// Generate inject the dependsOn of resources in a specified order.
 func (g *orderedResourcesGenerator) Generate(spec *models.Spec) error {
 	if spec.Resources == nil {
 		spec.Resources = make(models.Resources, 0)
 	}
-
 	for i := 0; i < len(spec.Resources); i++ {
 		if spec.Resources[i].Type != runtime.Kubernetes {
 			continue
 		}
-
 		curResource := &spec.Resources[i]
 		curKind := resourceKind(curResource)
-
 		dependKinds := g.findDependKinds(curKind)
 		injectAllDependsOn(curResource, dependKinds, spec.Resources)
 	}
-
 	return nil
 }
 
+// resourceKind returns the kind of the given resource.
 func resourceKind(r *models.Resource) string {
 	u := &unstructured.Unstructured{}
 	u.SetUnstructuredContent(r.Attributes)
 	return u.GetKind()
 }
 
+// injectAllDependsOn injects all dependsOn relationships for the given resource and dependent kinds.
 func injectAllDependsOn(curResource *models.Resource, dependKinds []string, rs []models.Resource) {
 	for _, dependKind := range dependKinds {
 		dependResources := findDependResources(dependKind, rs)
@@ -87,6 +90,7 @@ func injectAllDependsOn(curResource *models.Resource, dependKinds []string, rs [
 	}
 }
 
+// injectDependsOn injects dependsOn relationships for the given resource and dependent resources.
 func injectDependsOn(res *models.Resource, dependResources []*models.Resource) {
 	dependsOn := make([]string, 0, len(dependResources))
 	for _, r := range dependResources {
@@ -97,6 +101,7 @@ func injectDependsOn(res *models.Resource, dependResources []*models.Resource) {
 	}
 }
 
+// findDependResources returns the dependent resources of the specified kind.
 func findDependResources(dependKind string, rs []models.Resource) []*models.Resource {
 	var dependResources []*models.Resource
 	for i := 0; i < len(rs); i++ {
@@ -107,6 +112,7 @@ func findDependResources(dependKind string, rs []models.Resource) []*models.Reso
 	return dependResources
 }
 
+// findDependKinds returns the dependent resource kinds for the specified kind.
 func (g *orderedResourcesGenerator) findDependKinds(curKind string) []string {
 	dependKinds := make([]string, 0)
 	for _, previousKind := range g.orderedKinds {
