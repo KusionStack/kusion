9e498662caba7a93ea6e83162e75b375c8868208
elliotxx <951376975@qq.com>
2023-08-22 18:57:03 +0800

    feat: support default order of kubernetes resource

---
+++ pkg/generator/appconfiguration/generator/ordered_resources_generator.go
@@ -0,0 +1,118 @@
+package generator
+
+import (
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"kusionstack.io/kusion/pkg/engine/runtime"
+	"kusionstack.io/kusion/pkg/generator/appconfiguration"
+	"kusionstack.io/kusion/pkg/models"
+)
+
+var defaultOrderedKinds = []string{
+	"Namespace",
+	"ResourceQuota",
+	"StorageClass",
+	"CustomResourceDefinition",
+	"ServiceAccount",
+	"PodSecurityPolicy",
+	"Role",
+	"ClusterRole",
+	"RoleBinding",
+	"ClusterRoleBinding",
+	"ConfigMap",
+	"Secret",
+	"Endpoints",
+	"Service",
+	"LimitRange",
+	"PriorityClass",
+	"PersistentVolume",
+	"PersistentVolumeClaim",
+	"Deployment",
+	"StatefulSet",
+	"CronJob",
+	"PodDisruptionBudget",
+	"MutatingWebhookConfiguration",
+	"ValidatingWebhookConfiguration",
+}
+
+type orderedResourcesGenerator struct {
+	orderedKinds []string
+}
+
+func NewOrderedResourcesGenerator(orderedKinds []string) (appconfiguration.Generator, error) {
+	if len(orderedKinds) == 0 {
+		orderedKinds = defaultOrderedKinds
+	}
+	return &orderedResourcesGenerator{
+		orderedKinds: orderedKinds,
+	}, nil
+}
+
+func NewOrderedResourcesGeneratorFunc(orderedKinds []string) appconfiguration.NewGeneratorFunc {
+	return func() (appconfiguration.Generator, error) {
+		return NewOrderedResourcesGenerator(orderedKinds)
+	}
+}
+
+func (g *orderedResourcesGenerator) Generate(spec *models.Spec) error {
+	if spec.Resources == nil {
+		spec.Resources = make(models.Resources, 0)
+	}
+
+	for i := 0; i < len(spec.Resources); i++ {
+		if spec.Resources[i].Type != runtime.Kubernetes {
+			continue
+		}
+
+		curResource := &spec.Resources[i]
+		curKind := resourceKind(curResource)
+
+		dependKinds := g.findDependKinds(curKind)
+		injectAllDependsOn(curResource, dependKinds, spec.Resources)
+	}
+
+	return nil
+}
+
+func resourceKind(r *models.Resource) string {
+	u := &unstructured.Unstructured{}
+	u.SetUnstructuredContent(r.Attributes)
+	return u.GetKind()
+}
+
+func injectAllDependsOn(curResource *models.Resource, dependKinds []string, rs []models.Resource) {
+	for _, dependKind := range dependKinds {
+		dependResources := findDependResources(dependKind, rs)
+		injectDependsOn(curResource, dependResources)
+	}
+}
+
+func injectDependsOn(res *models.Resource, dependResources []*models.Resource) {
+	dependsOn := make([]string, 0, len(dependResources))
+	for _, r := range dependResources {
+		res.DependsOn = append(res.DependsOn, r.ID)
+	}
+	if len(dependsOn) > 0 {
+		res.DependsOn = dependsOn
+	}
+}
+
+func findDependResources(dependKind string, rs []models.Resource) []*models.Resource {
+	var dependResources []*models.Resource
+	for i := 0; i < len(rs); i++ {
+		if resourceKind(&rs[i]) == dependKind {
+			dependResources = append(dependResources, &rs[i])
+		}
+	}
+	return dependResources
+}
+
+func (g *orderedResourcesGenerator) findDependKinds(curKind string) []string {
+	var dependKinds []string
+	for _, previousKind := range g.orderedKinds {
+		if curKind == previousKind {
+			break
+		}
+		dependKinds = append(dependKinds, previousKind)
+	}
+	return dependKinds
+}
---
+++ pkg/generator/appconfiguration/generator/ordered_resources_generator_test.go
@@ -0,0 +1,157 @@
+package generator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"kusionstack.io/kusion/pkg/engine/runtime"
+	"kusionstack.io/kusion/pkg/models"
+)
+
+var (
+	fakeDeployment = map[string]interface{}{
+		"apiVersion": "apps/v1",
+		"kind":       "Deployment",
+		"metadata": map[string]interface{}{
+			"namespace": "foo",
+			"name":      "bar",
+		},
+		"spec": map[string]interface{}{
+			"replica": 1,
+			"template": map[string]interface{}{
+				"spec": map[string]interface{}{
+					"containers": []map[string]interface{}{
+						{
+							"image": "foo.bar.com:v1",
+							"name":  "bar",
+						},
+					},
+				},
+			},
+		},
+	}
+	fakeNamespace = map[string]interface{}{
+		"apiVersion": "v1",
+		"kind":       "Namespace",
+		"metadata": map[string]interface{}{
+			"name": "foo",
+		},
+	}
+	genOldSpec = func() *models.Spec {
+		return &models.Spec{
+			Resources: models.Resources{
+				{
+					ID:         "apps/v1:Deployment:foo:bar",
+					Type:       runtime.Kubernetes,
+					Attributes: fakeDeployment,
+				},
+				{
+					ID:         "v1:Namespace:foo",
+					Type:       runtime.Kubernetes,
+					Attributes: fakeNamespace,
+				},
+			},
+		}
+	}
+	genNewSpec = func() *models.Spec {
+		return &models.Spec{
+			Resources: models.Resources{
+				{
+					ID:         "apps/v1:Deployment:foo:bar",
+					Type:       runtime.Kubernetes,
+					Attributes: fakeDeployment,
+					DependsOn:  []string{"v1:Namespace:foo"},
+				},
+				{
+					ID:         "v1:Namespace:foo",
+					Type:       runtime.Kubernetes,
+					Attributes: fakeNamespace,
+				},
+			},
+		}
+	}
+)
+
+func TestOrderedResourcesGenerator_Generate(t *testing.T) {
+	orderedGenerator, err := NewOrderedResourcesGenerator(nil)
+	assert.NoError(t, err)
+
+	expected := genNewSpec()
+	actual := genOldSpec()
+	err = orderedGenerator.Generate(actual)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestResourceKind(t *testing.T) {
+	resource := &models.Resource{
+		Type: runtime.Kubernetes,
+		Attributes: map[string]interface{}{
+			"kind": "Deployment",
+		},
+	}
+
+	expected := "Deployment"
+	actual := resourceKind(resource)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestInjectAllDependsOn(t *testing.T) {
+	spec := genOldSpec()
+	dependKinds := []string{"Namespace"}
+
+	expected := []string{"v1:Namespace:foo"}
+	actual := []models.Resource(spec.Resources)[0]
+	injectAllDependsOn(&actual, dependKinds, spec.Resources)
+
+	assert.Equal(t, expected, actual.DependsOn)
+}
+
+func TestFindDependKinds(t *testing.T) {
+	curKind := "Deployment"
+	g := &orderedResourcesGenerator{
+		orderedKinds: defaultOrderedKinds,
+	}
+
+	expected := []string{
+		"Namespace",
+		"ResourceQuota",
+		"StorageClass",
+		"CustomResourceDefinition",
+		"ServiceAccount",
+		"PodSecurityPolicy",
+		"Role",
+		"ClusterRole",
+		"RoleBinding",
+		"ClusterRoleBinding",
+		"ConfigMap",
+		"Secret",
+		"Endpoints",
+		"Service",
+		"LimitRange",
+		"PriorityClass",
+		"PersistentVolume",
+		"PersistentVolumeClaim",
+	}
+	actual := g.findDependKinds(curKind)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestFindDependResources(t *testing.T) {
+	dependKind := "Namespace"
+	resources := genOldSpec().Resources
+
+	expected := []*models.Resource{
+		{
+			ID:         "v1:Namespace:foo",
+			Type:       runtime.Kubernetes,
+			Attributes: fakeNamespace,
+		},
+	}
+	actual := findDependResources(dependKind, resources)
+
+	assert.Equal(t, expected, actual)
+}
