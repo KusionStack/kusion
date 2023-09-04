6d61d87ec9007248d3a93ba35ee5d5aa62fec693
liu-hm19 <liuhaoming.lhm@antgroup.com>
2023-09-04 17:00:25 +0800

    feat: add database generator (#435)

---
+++ pkg/generator/appconfiguration/generator/accessories/database/alicloud_rds.go
@@ -0,0 +1,159 @@
+package accessories
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+	"kusionstack.io/kusion/pkg/generator/appconfiguration"
+	"kusionstack.io/kusion/pkg/models"
+	"kusionstack.io/kusion/pkg/models/appconfiguration/accessories/database"
+)
+
+const (
+	alicloudDBInstance      = "alicloud_db_instance"
+	alicloudDBConnection    = "alicloud_db_connection"
+	alicloudRDSAccount      = "alicloud_rds_account"
+	defaultAlicloudProvider = "registry.terraform.io/aliyun/alicloud/1.209.0"
+)
+
+var (
+	tfProviderAlicloud     = os.Getenv("TF_PROVIDER_ALICLOUD")
+	alicloudProviderRegion = os.Getenv("ALICLOUD_PROVIDER_REGION")
+)
+
+type alicloudServerlessConfig struct {
+	AutoPause   bool `yaml:"auto_pause" json:"auto_pause"`
+	SwitchForce bool `yaml:"switch_force" json:"switch_force"`
+	MaxCapacity int  `yaml:"max_capacity,omitempty" json:"max_capacity,omitempty"`
+	MinCapacity int  `yaml:"min_capacity,omitempty" json:"min_capacity,omitempty"`
+}
+
+func (g *databaseGenerator) generateAlicloudResources(db *database.Database, spec *models.Spec) (*v1.Secret, error) {
+	// Set the terraform random and alicloud provider.
+	randomProvider := &models.Provider{}
+	if err := randomProvider.SetString(randomProviderURL); err != nil {
+		return nil, err
+	}
+
+	// The region of the alicloud provider must be set.
+	if alicloudProviderRegion == "" {
+		return nil, fmt.Errorf("the region of the alicloud provider must be set")
+	}
+
+	var providerURL string
+	alicloudProvider := &models.Provider{}
+	if tfProviderAlicloud == "" {
+		providerURL = defaultAlicloudProvider
+	} else {
+		providerURL = tfProviderAlicloud
+	}
+
+	if err := alicloudProvider.SetString(providerURL); err != nil {
+		return nil, err
+	}
+
+	// Build alicloud_db_instance.
+	alicloudDBInstanceID, r := g.generateAlicloudDBInstance(alicloudProviderRegion, alicloudProvider, db)
+	spec.Resources = append(spec.Resources, r)
+
+	// Build alicloud_db_connection for alicloud_db_instance.
+	var alicloudDBConnectionID string
+	if isPublicAccessible(db.SecurityIPs) {
+		alicloudDBConnectionID, r = g.generateAlicloudDBConnection(alicloudDBInstanceID, alicloudProviderRegion, alicloudProvider, db)
+		spec.Resources = append(spec.Resources, r)
+	}
+
+	// Build random_password for alicloud_rds_account.
+	randomPasswordID, r := g.generateTFRandomPassword(randomProvider)
+	spec.Resources = append(spec.Resources, r)
+
+	// Build alicloud_rds_account.
+	r = g.generateAlicloudRDSAccount(db.Username, randomPasswordID, alicloudDBInstanceID, alicloudProviderRegion, alicloudProvider, db)
+	spec.Resources = append(spec.Resources, r)
+
+	// Inject the host address, username and password into k8s secret.
+	password := appconfiguration.KusionPathDependency(randomPasswordID, "result")
+	hostAddress := appconfiguration.KusionPathDependency(alicloudDBInstanceID, "connection_string")
+	if !db.PrivateRouting {
+		// Set the public network connection string as the host address.
+		hostAddress = appconfiguration.KusionPathDependency(alicloudDBConnectionID, "connection_string")
+	}
+
+	return g.generateDBSecret(hostAddress, db.Username, password, spec)
+}
+
+func (g *databaseGenerator) generateAlicloudDBInstance(region string,
+	provider *models.Provider, db *database.Database,
+) (string, models.Resource) {
+	dbAttrs := map[string]interface{}{
+		"category":         db.Category,
+		"engine":           db.Engine,
+		"engine_version":   db.Version,
+		"instance_storage": db.Size,
+		"instance_type":    db.InstanceType,
+		"security_ips":     db.SecurityIPs,
+		"vswitch_id":       db.SubnetID,
+	}
+
+	// Set serverless specific attributes.
+	if strings.Contains(db.Category, "serverless") {
+		dbAttrs["db_instance_storage_type"] = "cloud_essd"
+		dbAttrs["instance_charge_type"] = "Serverless"
+
+		serverlessConfig := alicloudServerlessConfig{
+			MaxCapacity: 8,
+			MinCapacity: 1,
+		}
+		if db.Engine == "SQLServer" {
+			serverlessConfig.MinCapacity = 2
+		} else if db.Engine == "MySQL" {
+			serverlessConfig.AutoPause = false
+			serverlessConfig.SwitchForce = false
+		}
+		dbAttrs["serverless_config"] = []alicloudServerlessConfig{
+			serverlessConfig,
+		}
+	}
+
+	id := appconfiguration.TerraformResourceID(provider, alicloudDBInstance, g.appName)
+	pvdExts := appconfiguration.ProviderExtensions(provider, map[string]any{
+		"region": region,
+	}, alicloudDBInstance)
+
+	return id, appconfiguration.TerraformResource(id, nil, dbAttrs, pvdExts)
+}
+
+func (g *databaseGenerator) generateAlicloudDBConnection(dbInstanceID, region string,
+	provider *models.Provider, db *database.Database,
+) (string, models.Resource) {
+	dbConnectionAttrs := map[string]interface{}{
+		"instance_id": appconfiguration.KusionPathDependency(dbInstanceID, "id"),
+	}
+
+	id := appconfiguration.TerraformResourceID(provider, alicloudDBConnection, g.appName)
+	pvdExts := appconfiguration.ProviderExtensions(provider, map[string]any{
+		"region": region,
+	}, alicloudDBConnection)
+
+	return id, appconfiguration.TerraformResource(id, nil, dbConnectionAttrs, pvdExts)
+}
+
+func (g *databaseGenerator) generateAlicloudRDSAccount(accountName, randomPasswordID, dbInstanceID, region string,
+	provider *models.Provider, db *database.Database,
+) models.Resource {
+	rdsAccountAttrs := map[string]interface{}{
+		"account_name":     accountName,
+		"account_password": appconfiguration.KusionPathDependency(randomPasswordID, "result"),
+		"account_type":     "Super",
+		"db_instance_id":   appconfiguration.KusionPathDependency(dbInstanceID, "id"),
+	}
+
+	id := appconfiguration.TerraformResourceID(provider, alicloudRDSAccount, g.appName)
+	pvdExts := appconfiguration.ProviderExtensions(provider, map[string]any{
+		"region": region,
+	}, alicloudRDSAccount)
+
+	return appconfiguration.TerraformResource(id, nil, rdsAccountAttrs, pvdExts)
+}
---
+++ pkg/generator/appconfiguration/generator/accessories/database/database_generator.go
@@ -0,0 +1,189 @@
+package accessories
+
+import (
+	"fmt"
+	"net"
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"kusionstack.io/kusion/pkg/generator/appconfiguration"
+	"kusionstack.io/kusion/pkg/models"
+	"kusionstack.io/kusion/pkg/models/appconfiguration/accessories/database"
+	"kusionstack.io/kusion/pkg/models/appconfiguration/workload"
+	"kusionstack.io/kusion/pkg/projectstack"
+)
+
+const (
+	dbResSuffix       = "-db"
+	randomPassword    = "random_password"
+	randomProviderURL = "registry.terraform.io/hashicorp/random/3.5.1"
+	dbHostAddressEnv  = "KUSION_DB_HOST"
+	dbUsernameEnv     = "KUSION_DB_USERNAME"
+	dbPasswordEnv     = "KUSION_DB_PASSWORD"
+)
+
+type databaseGenerator struct {
+	project  *projectstack.Project
+	stack    *projectstack.Stack
+	appName  string
+	workload *workload.Workload
+	database *database.Database
+}
+
+func NewDatabaseGenerator(
+	project *projectstack.Project,
+	stack *projectstack.Stack,
+	appName string,
+	workload *workload.Workload,
+	database *database.Database,
+) (appconfiguration.Generator, error) {
+	if len(project.Name) == 0 {
+		return nil, fmt.Errorf("project name must not be empty")
+	}
+
+	return &databaseGenerator{
+		project:  project,
+		stack:    stack,
+		appName:  appName,
+		workload: workload,
+		database: database,
+	}, nil
+}
+
+func NewDatabaseGeneratorFunc(
+	project *projectstack.Project,
+	stack *projectstack.Stack,
+	appName string,
+	workload *workload.Workload,
+	database *database.Database,
+) appconfiguration.NewGeneratorFunc {
+	return func() (appconfiguration.Generator, error) {
+		return NewDatabaseGenerator(project, stack, appName, workload, database)
+	}
+}
+
+func (g *databaseGenerator) Generate(spec *models.Spec) error {
+	if spec.Resources == nil {
+		spec.Resources = make(models.Resources, 0)
+	}
+
+	// Skip rendering for empty database instance.
+	db := g.database
+	if db == nil {
+		return nil
+	}
+
+	var secret *v1.Secret
+	var err error
+	// Generate the database resources based on the type.
+	switch strings.ToLower(db.Type) {
+	case "aws":
+		secret, err = g.generateAWSResources(db, spec)
+	case "alicloud":
+		secret, err = g.generateAlicloudResources(db, spec)
+	default:
+		return fmt.Errorf("unsupported database type: %s", db.Type)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// Inject the database host address, username and password into the containers
+	// of the workload as environment variables with Kubernetes Secret.
+	return g.injectSecret(secret)
+}
+
+func (g *databaseGenerator) injectSecret(secret *v1.Secret) error {
+	// Inject the database information into the containers of service workload.
+	if g.workload.Service != nil {
+		for _, v := range g.workload.Service.Containers {
+			v.Env[dbHostAddressEnv] = "secret://" + secret.Name + "/hostAddress"
+			v.Env[dbUsernameEnv] = "secret://" + secret.Name + "/username"
+			v.Env[dbPasswordEnv] = "secret://" + secret.Name + "/password"
+		}
+	}
+
+	// Inject the database information into the containers of job workload.
+	if g.workload.Job != nil {
+		for _, v := range g.workload.Job.Containers {
+			v.Env[dbHostAddressEnv] = "secret://" + secret.Name + "/hostAddress"
+			v.Env[dbUsernameEnv] = "secret://" + secret.Name + "/username"
+			v.Env[dbPasswordEnv] = "secret://" + secret.Name + "/password"
+		}
+	}
+
+	return nil
+}
+
+func (g *databaseGenerator) generateTFRandomPassword(provider *models.Provider) (string, models.Resource) {
+	pswAttrs := map[string]interface{}{
+		"length":           16,
+		"special":          true,
+		"override_special": "_",
+	}
+
+	id := appconfiguration.TerraformResourceID(provider, randomPassword, g.appName+dbResSuffix)
+	pvdExts := appconfiguration.ProviderExtensions(provider, nil, randomPassword)
+
+	return id, appconfiguration.TerraformResource(id, nil, pswAttrs, pvdExts)
+}
+
+func (g *databaseGenerator) generateDBSecret(hostAddress, username, password string, spec *models.Spec) (*v1.Secret, error) {
+	// Create the data map of k8s secret storing the database host address, username
+	// and password.
+	data := make(map[string]string)
+	data["hostAddress"] = hostAddress
+	data["username"] = username
+	data["password"] = password
+
+	// Create the k8s secret and append to the spec.
+	secret := &v1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: v1.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      g.appName + dbResSuffix,
+			Namespace: g.project.Name,
+		},
+		StringData: data,
+	}
+
+	return secret, appconfiguration.AppendToSpec(
+		models.Kubernetes,
+		appconfiguration.KubernetesResourceID(secret.TypeMeta, secret.ObjectMeta),
+		spec,
+		secret,
+	)
+}
+
+func isPublicAccessible(securityIPs []string) bool {
+	var parsedIP net.IP
+	for _, ip := range securityIPs {
+		if isIPAddress(ip) {
+			parsedIP = net.ParseIP(ip)
+		} else if isCIDR(ip) {
+			parsedIP, _, _ = net.ParseCIDR(ip)
+		}
+
+		if parsedIP != nil && !parsedIP.IsPrivate() {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isIPAddress(ipStr string) bool {
+	ip := net.ParseIP(ipStr)
+
+	return ip != nil
+}
+
+func isCIDR(cidrStr string) bool {
+	_, _, err := net.ParseCIDR(cidrStr)
+
+	return err == nil
+}
