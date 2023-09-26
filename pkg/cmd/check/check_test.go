531600980446ebae1515209bb0a70804e5139a00
Mrcopytuo <920411694@qq.com>
2023-09-26 10:13:57 +0800

    fix: check test format

--- pkg/cmd/check/check_test.go
+++ pkg/cmd/check/check_test.go
@@ -11,15 +11,13 @@ import (
 
 func TestNewCmdCheck(t *testing.T) {
 	mockey.PatchConvey("", t, func() {
-
-		mockey.Mock((*compile.CompileOptions).Complete).To(func(o *compile.CompileOptions, args []string) error {
+		mockey.Mock((*compile.Options).Complete).To(func(o *compile.Options, args []string) error {
 			o.Output = "stdout"
 			return nil
 		}).Build()
-		mockey.Mock((*compile.CompileOptions).Run).To(func(*compile.CompileOptions) error {
+		mockey.Mock((*compile.Options).Run).To(func(*compile.Options) error {
 			return nil
 		}).Build()
-
 		cmd := NewCmdCheck()
 		err := cmd.Execute()
 		assert.Nil(t, err)
