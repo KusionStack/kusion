55eac7e76a52a3b4fd5bb9ce1525dc7851faf365
TonyAdo <71679464+adohe@users.noreply.github.com>
2023-11-21 15:15:26 +0800

    refactor: update secret generator for upcoming external secret (#607)

---
+++ pkg/generator/appconfiguration/generator/workload/secret/secret_generator_test.go
@@ -0,0 +1,98 @@
+package secret
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"kusionstack.io/kusion/pkg/models"
+	"kusionstack.io/kusion/pkg/models/appconfiguration/workload"
+	"kusionstack.io/kusion/pkg/projectstack"
+)
+
+func TestGenerateSecret(t *testing.T) {
+	tests := map[string]struct {
+		secretName string
+		secretType string
+		secretData map[string]string
+
+		expectErr string
+	}{
+		"create_basic_auth_secret": {
+			secretName: "secret-basic-auth",
+			secretType: "basic",
+			secretData: map[string]string{
+				"username": "admin",
+				"password": "t0p-Secret",
+			},
+		},
+		"create_basic_auth_secret_empty_input": {
+			secretName: "secret-basic-auth",
+			secretType: "basic",
+			secretData: map[string]string{},
+		},
+		"create_token_secret": {
+			secretName: "secret-token",
+			secretType: "token",
+			secretData: map[string]string{
+				"token": "YmFyCg==",
+			},
+		},
+		"create_token_secret_empty_input": {
+			secretName: "secret-token",
+			secretType: "token",
+			secretData: map[string]string{},
+		},
+		"create_opaque_secret": {
+			secretName: "empty-secret",
+			secretType: "opaque",
+			secretData: map[string]string{},
+		},
+		"create_opaque_secret_any_info": {
+			secretName: "empty-secret",
+			secretType: "opaque",
+			secretData: map[string]string{
+				"accessKey": "dHJ1ZQ==",
+			},
+		},
+		"create_certificate_secret": {
+			secretName: "secret-tls",
+			secretType: "certificate",
+			secretData: map[string]string{
+				"tls.crt": "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUNVakNDQWJz",
+				"tls.key": "RXhhbXBsZSBkYXRhIGZvciB0aGUgVExTIGNydCBmaWVsZA==",
+			},
+		},
+		"create_invalid_secret_invalid_type": {
+			secretName: "invalid-tls",
+			secretType: "cred",
+			expectErr:  "unrecognized secret type cred",
+		},
+	}
+
+	project := &projectstack.Project{
+		ProjectConfiguration: projectstack.ProjectConfiguration{
+			Name: "helloworld",
+		},
+	}
+	// run all the tests
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			secrets := map[string]workload.Secret{
+				name: {
+					Type: test.secretType,
+					Data: test.secretData,
+				},
+			}
+			intent := &models.Intent{}
+			generator, _ := NewSecretGenerator(project, secrets)
+			err := generator.Generate(intent)
+			if test.expectErr == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.EqualError(t, err, test.expectErr)
+			}
+		})
+	}
+}
--- pkg/models/appconfiguration/workload/secret.go
+++ pkg/models/appconfiguration/workload/secret.go
@@ -1,10 +1,7 @@
 package workload
 
-import v1 "k8s.io/api/core/v1"
-
 type Secret struct {
-	// Image to run for this container
-	Type      v1.SecretType
+	Type      string            `yaml:"type" json:"type"`
 	Data      map[string]string `yaml:"data,omitempty" json:"data,omitempty"`
 	Immutable bool              `yaml:"immutable,omitempty" json:"immutable,omitempty"`
 }
