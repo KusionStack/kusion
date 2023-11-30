267ff30a41c7afc49e4d3b4e68cb6081666d7dfe
healthjyk <healthjyk@163.com>
2023-11-30 17:15:41 +0800

    feat: move GenericConfig to workspace.go

--- pkg/apis/workspace/util.go
+++
@@ -1,5 +0,0 @@
-package workspace
-
-// GenericConfig is a generic model to describe config which shields the difference among multiple concrete
-// models. GenericConfig is designed for extensibility, used for module, terraform runtime config, etc.
-type GenericConfig map[string]any
--- pkg/apis/workspace/workspace.go
+++ pkg/apis/workspace/workspace.go
@@ -17,3 +17,7 @@ type Workspace struct {
 	// Backends are the configs of a set of backends.
 	Backends BackendConfigs `yaml:"backends,omitempty" json:"backends,omitempty"`
 }
+
+// GenericConfig is a generic model to describe config which shields the difference among multiple concrete
+// models. GenericConfig is designed for extensibility, used for module, terraform runtime config, etc.
+type GenericConfig map[string]any
