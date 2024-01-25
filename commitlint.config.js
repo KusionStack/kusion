2a932dea7b9ce6e2edc5777788cbeeda271da581
healthjyk <healthjyk@163.com>
2024-01-25 20:35:43 +0800

    feat: allow commit body line longer than 100

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,12 @@
 //
 // Rules can be referenced: 
 // https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
-module.exports = {extends: ['@commitlint/config-conventional']}
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+
+    rules: {
+        'body-max-line-length': [2, 'always', 500],
+    },
+};
+
+module.exports = Configuration;
