80ad349049a1b89c37d8185d69a05efc8c05f90d
KK <68334452+healthjyk@users.noreply.github.com>
2024-03-04 14:06:18 +0800

    feat: add specified structured backend config models (#842)

--- pkg/apis/core/v1/config.go
+++ pkg/apis/core/v1/config.go
@@ -1,17 +1,35 @@
 package v1
 
-const (
-	BackendTypeLocal = "local"
-	BackendTypeMysql = "mysql"
-	BackendTypeOss   = "oss"
-	BackendTypeS3    = "s3"
-)
+const ConfigBackends = "backends"
 
 // Config contains configurations for kusion cli, which stores in ${KUSION_HOME}/config.yaml.
 type Config struct {
 	// Backends contains the configurations for multiple backends.
 	Backends *BackendConfigs `yaml:"backends,omitempty"`
 }
+
+const (
+	BackendCurrent            = "current"
+	BackendType               = "type"
+	BackendConfigItems        = "configs"
+	BackendLocalPath          = "path"
+	BackendMysqlDBName        = "dbName"
+	BackendMysqlUser          = "user"
+	BackendMysqlPassword      = "password"
+	BackendMysqlHost          = "host"
+	BackendMysqlPort          = "port"
+	BackendGenericOssEndpoint = "endpoint"
+	BackendGenericOssAK       = "accessKeyID"
+	BackendGenericOssSK       = "accessKeySecret"
+	BackendGenericOssBucket   = "bucket"
+	BackendGenericOssPrefix   = "prefix"
+	BackendS3Region           = "region"
+
+	BackendTypeLocal = "local"
+	BackendTypeMysql = "mysql"
+	BackendTypeOss   = "oss"
+	BackendTypeS3    = "s3"
+)
 
 // BackendConfigs contains the configuration of multiple backends and the current backend.
 type BackendConfigs struct {
@@ -22,11 +40,132 @@ type BackendConfigs struct {
 	Backends map[string]*BackendConfig `yaml:"backends,omitempty,inline"`
 }
 
-// BackendConfig contains the type and configs of a backend, which is used to store State and Workspace.
+// BackendConfig contains the type and configs of a backend, which is used to store Spec, State and Workspace.
 type BackendConfig struct {
 	// Type is the backend type, supports BackendTypeLocal, BackendTypeMysql, BackendTypeOss, BackendTypeS3.
 	Type string `yaml:"type,omitempty"`
 
 	// Configs contains config items of the backend, whose keys differ from different backend types.
-	Configs map[string]string `yaml:"configs,omitempty"`
+	Configs map[string]any `yaml:"configs,omitempty"`
+}
+
+// BackendLocalConfig contains the config of using local file system as backend, which can be converted
+// from BackendConfig if Type is BackendTypeLocal.
+type BackendLocalConfig struct {
+	// Path of the directory to store the files.
+	Path string
+}
+
+// BackendMysqlConfig contains the config of using mysql database as backend, which can be converted
+// from BackendConfig if Type is BackendMysqlConfig.
+type BackendMysqlConfig struct {
+	// DBName is the database name.
+	DBName string `yaml:"dbName" json:"dbName"`
+
+	// User of the database.
+	User string `yaml:"user" json:"user"`
+
+	// Password of the database.
+	Password string `yaml:"password,omitempty" json:"password,omitempty"`
+
+	// Host of the database.
+	Host string `yaml:"host" json:"host"`
+
+	// Port of the database. If not set, then it will be set to DefaultMysqlPort.
+	Port int `yaml:"port,omitempty" json:"port,omitempty"`
+}
+
+// BackendOssConfig contains the config of using OSS as backend, which can be converted from BackendConfig
+// if Type is BackendOssConfig.
+type BackendOssConfig struct {
+	*GenericBackendObjectStorageConfig `yaml:",inline" json:",inline"` // OSS asks for non-empty endpoint
+}
+
+// BackendS3Config contains the config of using S3 as backend, which can be converted from BackendConfig
+// if Type is BackendS3Config.
+type BackendS3Config struct {
+	*GenericBackendObjectStorageConfig `yaml:",inline" json:",inline"`
+
+	// Region of S3.
+	Region string `yaml:"region,omitempty" json:"region,omitempty"`
+}
+
+// GenericBackendObjectStorageConfig contains generic configs which can be reused by BackendOssConfig and
+// BackendS3Config.
+type GenericBackendObjectStorageConfig struct {
+	// Endpoint of the object storage service.
+	Endpoint string `yaml:"endpoint,omitempty" json:"endpoint,omitempty"`
+
+	// AccessKeyID of the object storage service.
+	AccessKeyID string `yaml:"accessKeyID,omitempty" json:"accessKeyID,omitempty"`
+
+	// AccessKeySecret of the object storage service.
+	AccessKeySecret string `yaml:"accessKeySecret,omitempty" json:"accessKeySecret,omitempty"`
+
+	// Bucket of the object storage service.
+	Bucket string `yaml:"bucket" json:"bucket"`
+
+	// Prefix of the key to store the files.
+	Prefix string `yaml:"prefix,omitempty" json:"prefix,omitempty"`
+}
+
+// ToLocalBackend converts BackendConfig to structured BackendLocalConfig, works only when the Type
+// is BackendTypeLocal, and the Configs are with correct type, or return nil.
+func (b *BackendConfig) ToLocalBackend() *BackendLocalConfig {
+	if b.Type != BackendTypeLocal {
+		return nil
+	}
+	return &BackendLocalConfig{
+		Path: b.Configs[BackendLocalPath].(string),
+	}
+}
+
+// ToMysqlBackend converts BackendConfig to structured BackendMysqlConfig, works only when the Type
+// is BackendTypeMysql, and the Configs are with correct type, or return nil.
+func (b *BackendConfig) ToMysqlBackend() *BackendMysqlConfig {
+	if b.Type != BackendTypeMysql {
+		return nil
+	}
+	return &BackendMysqlConfig{
+		DBName:   b.Configs[BackendMysqlDBName].(string),
+		User:     b.Configs[BackendMysqlUser].(string),
+		Password: b.Configs[BackendMysqlPassword].(string),
+		Host:     b.Configs[BackendMysqlHost].(string),
+		Port:     b.Configs[BackendMysqlPort].(int),
+	}
+}
+
+// ToOssBackend converts BackendConfig to structured BackendOssConfig, works only when the Type is
+// BackendTypeOss, and the Configs are with correct type, or return nil.
+func (b *BackendConfig) ToOssBackend() *BackendOssConfig {
+	if b.Type != BackendTypeOss {
+		return nil
+	}
+	return &BackendOssConfig{
+		&GenericBackendObjectStorageConfig{
+			Endpoint:        b.Configs[BackendGenericOssEndpoint].(string),
+			AccessKeyID:     b.Configs[BackendGenericOssAK].(string),
+			AccessKeySecret: b.Configs[BackendGenericOssSK].(string),
+			Bucket:          b.Configs[BackendGenericOssBucket].(string),
+			Prefix:          b.Configs[BackendGenericOssPrefix].(string),
+		},
+	}
+}
+
+// ToS3Backend converts BackendConfig to structured BackendS3Config, works only when the Type is
+// BackendTypeS3, and the Configs are with correct type, or return nil.
+func (b *BackendConfig) ToS3Backend() *BackendS3Config {
+	if b.Type != BackendTypeS3 {
+		return nil
+	}
+	return &BackendS3Config{
+		GenericBackendObjectStorageConfig: &GenericBackendObjectStorageConfig{
+			Endpoint:        b.Configs[BackendGenericOssEndpoint].(string),
+			AccessKeyID:     b.Configs[BackendGenericOssAK].(string),
+			AccessKeySecret: b.Configs[BackendGenericOssSK].(string),
+			Bucket:          b.Configs[BackendGenericOssBucket].(string),
+			Prefix:          b.Configs[BackendGenericOssPrefix].(string),
+		},
+		Region: b.Configs[BackendS3Region].(string),
+	}
 }
