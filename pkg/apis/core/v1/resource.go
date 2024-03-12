991f115877d639f395a70afefbb2c65b6ddd1d25
liuhaoming <liuhaoming.lhm@antgroup.com>
2024-03-12 17:59:44 +0800

    chore: add some code comments

--- pkg/apis/core/v1/resource.go
+++ pkg/apis/core/v1/resource.go
@@ -32,9 +32,12 @@ type Resource struct {
 }
 
 type Patcher struct {
-	Environments []v1.EnvVar       `json:"environments" yaml:"environments"`
-	Labels       map[string]string `json:"labels" yaml:"labels"`
-	Annotations  map[string]string `json:"annotations" yaml:"annotations"`
+	// Environments represent the environment variables patched to all containers in the workload.
+	Environments []v1.EnvVar `json:"environments" yaml:"environments"`
+	// Labels represent the labels patched to both the workload and pod.
+	Labels map[string]string `json:"labels" yaml:"labels"`
+	// Annotations represent the annotations patched to both the workload and pod.
+	Annotations map[string]string `json:"annotations" yaml:"annotations"`
 }
 
 func (r *Resource) ResourceKey() string {
