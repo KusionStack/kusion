11a2766396deca78e1d41c8b92e575a83700bd5a
healthjyk <healthjyk@163.com>
2024-03-19 14:39:13 +0800

    feat: delete deprecated workspace operator codes and refactor e2e test

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -7,9 +7,6 @@ import (
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
-
-	v1 "kusionstack.io/kusion/pkg/apis/core/v1"
-	"kusionstack.io/kusion/pkg/workspace"
 )
 
 func TestE2e(t *testing.T) {
@@ -35,11 +32,6 @@ var _ = ginkgo.BeforeSuite(func() {
 	ginkgo.By("kusion init", func() {
 		path := filepath.Join(GetWorkDir(), "konfig")
 		_, err := ExecKusionWithStdin("kusion init --online=true --yes=true", path, "\n")
-		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-	})
-
-	ginkgo.By("create sample workspace", func() {
-		err := createSampleWorkspace()
 		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 	})
 })
@@ -68,54 +60,4 @@ var _ = ginkgo.AfterSuite(func() {
 		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
 		gomega.Expect(output).To(gomega.BeEmpty())
 	})
-
-	ginkgo.By("clean up sample workspace", func() {
-		err := deleteSampleWorkspace()
-		gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
-	})
 })
-
-// createSampleWorkspace creates a sample workspace under default local path.
-// todo: 1, add REAL sample workspace in repo Konfig after splitting work; 2, use cli to create workspace after cli R&D work.
-func createSampleWorkspace() error {
-	wsOperator, err := workspace.NewDefaultOperator()
-	if err != nil {
-		return err
-	}
-	ws := &v1.Workspace{
-		Name: "dev",
-		// comment out for now, as download modules from remote repo is not supported yet.
-		// Modules: v1.ModuleConfigs{
-		//	"database": {
-		//		Default: v1.GenericConfig{
-		//			"type":         "aws",
-		//			"version":      "5.7",
-		//			"instanceType": "db.t3.micro",
-		//		},
-		//		ModulePatcherConfigs: v1.ModulePatcherConfigs{
-		//			"smallClass": {
-		//				GenericConfig: v1.GenericConfig{
-		//					"instanceType": "db.t3.small",
-		//				},
-		//				ProjectSelector: []string{"foo", "bar"},
-		//			},
-		//		},
-		//	},
-		//	"port": {
-		//		Default: v1.GenericConfig{
-		//			"type": "aws",
-		//		},
-		//	},
-		// },
-	}
-	return wsOperator.CreateWorkspace(ws)
-}
-
-// deleteSampleWorkspace deletes the sample workspace.
-func deleteSampleWorkspace() error {
-	wsOperator, err := workspace.NewDefaultOperator()
-	if err != nil {
-		return err
-	}
-	return wsOperator.DeleteWorkspace("dev")
-}
