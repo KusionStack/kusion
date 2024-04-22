cbd6cfd5a554aa4b28c280457c53dad8be44e1ac
liu-hm19 <liuhaoming.lhm@antgroup.com>
2024-04-22 17:48:18 +0800

    feat: apply supports port forwarding (#1057)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module kusionstack.io/kusion
 
-go 1.22
+go 1.22.1
 
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.4
@@ -50,31 +50,31 @@ require (
 	github.com/jinzhu/copier v0.3.2
 	github.com/lucasb-eyer/go-colorful v1.0.3
 	github.com/mitchellh/hashstructure v1.0.0
-	github.com/onsi/ginkgo/v2 v2.13.0
-	github.com/onsi/gomega v1.30.0
+	github.com/onsi/ginkgo/v2 v2.15.0
+	github.com/onsi/gomega v1.31.0
 	github.com/pkg/errors v0.9.1
 	github.com/pterm/pterm v0.12.60
 	github.com/pulumi/pulumi/sdk/v3 v3.68.0
 	github.com/sergi/go-diff v1.3.1
 	github.com/spf13/afero v1.6.0
-	github.com/spf13/cobra v1.7.0
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.9.0
 	github.com/swaggo/http-swagger v1.3.4
 	github.com/swaggo/swag v1.16.3
 	github.com/texttheater/golang-levenshtein v1.0.1
 	github.com/tidwall/gjson v1.17.0
 	github.com/zclconf/go-cty v1.12.1
-	go.uber.org/zap v1.24.0
+	go.uber.org/zap v1.27.0
 	golang.org/x/exp v0.0.0-20220909182711-5c715a9e8561
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/mysql v1.5.4
 	gorm.io/gorm v1.25.7
-	k8s.io/api v0.29.2
-	k8s.io/apimachinery v0.29.2
+	k8s.io/api v0.30.0
+	k8s.io/apimachinery v0.30.0
 	k8s.io/cli-runtime v0.29.2
-	k8s.io/client-go v0.29.2
+	k8s.io/client-go v0.30.0
 	k8s.io/component-base v0.27.2
 	k8s.io/kubectl v0.27.1
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b
@@ -206,7 +206,7 @@ require (
 	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/containerd/containerd v1.7.6 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/dimchansky/utfbom v1.1.1 // indirect
 	github.com/docker/cli v24.0.6+incompatible // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
@@ -221,7 +221,7 @@ require (
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
-	github.com/go-logr/logr v1.3.0
+	github.com/go-logr/logr v1.4.1
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
@@ -312,12 +312,11 @@ require (
 	go.opentelemetry.io/otel v1.21.0 // indirect
 	go.opentelemetry.io/otel/metric v1.21.0 // indirect
 	go.opentelemetry.io/otel/trace v1.21.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.1.0 // indirect
 	golang.org/x/crypto v0.21.0
 	golang.org/x/mod v0.16.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/oauth2 v0.16.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
@@ -332,8 +331,8 @@ require (
 	google.golang.org/protobuf v1.33.0
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
-	k8s.io/klog/v2 v2.110.1
-	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
+	k8s.io/klog/v2 v2.120.1
+	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	oras.land/oras-go/v2 v2.3.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
---
+++ pkg/engine/operation/port_forward.go
@@ -0,0 +1,217 @@
+package operation
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+
+	yamlv2 "gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	k8syaml "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	v1 "kusionstack.io/kusion/pkg/apis/api.kusion.io/v1"
+	"kusionstack.io/kusion/pkg/engine/operation/models"
+	"kusionstack.io/kusion/pkg/engine/printers/convertor"
+	"kusionstack.io/kusion/pkg/engine/runtime/kubernetes/kubeops"
+)
+
+var (
+	ErrEmptySpec               = errors.New("empty resources in spec")
+	ErrEmptyService            = errors.New("empty k8s service")
+	ErrNotOneSvcWithTargetPort = errors.New("only support one k8s service to forward with target port")
+)
+
+type PortForwardOperation struct {
+	models.Operation
+}
+
+type PortForwardRequest struct {
+	models.Request `json:",inline" yaml:",inline"`
+	Port           int
+}
+
+func (bpo *PortForwardOperation) PortForward(req *PortForwardRequest) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if req.Intent == nil {
+		return ErrEmptySpec
+	}
+
+	// Find Kubernetes Service in the resources of Spec.
+	services := make(map[*v1.Resource]*corev1.Service)
+	for _, res := range req.Intent.Resources {
+		// Skip non-Kubernetes resources.
+		if res.Type != v1.Kubernetes {
+			continue
+		}
+
+		// Convert interface{} to unstructured.
+		rYaml, err := yamlv2.Marshal(res.Attributes)
+		if err != nil {
+			return fmt.Errorf("failed to convert resource attributes to unstructured raw yaml: %v", err)
+		}
+
+		// Decode YAML manifest into unstructured.Unstructured.
+		decUnstructured := k8syaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+		obj := &unstructured.Unstructured{}
+
+		_, _, err = decUnstructured.Decode(rYaml, nil, obj)
+		if err != nil {
+			return fmt.Errorf("failed to decode yaml manifest into unstructured object: %v", err)
+		}
+
+		if obj.GetKind() != convertor.Service {
+			continue
+		}
+
+		convertedObj := convertor.ToK8s(obj)
+		services[&res] = convertedObj.(*corev1.Service)
+	}
+
+	if len(services) == 0 {
+		return ErrEmptyService
+	}
+
+	filteredServices := make(map[*v1.Resource]*corev1.Service)
+	for res, svc := range services {
+		targetPortFound := false
+		for _, port := range svc.Spec.Ports {
+			if port.Port == int32(req.Port) {
+				targetPortFound = true
+				continue
+			}
+		}
+
+		if targetPortFound {
+			filteredServices[res] = svc
+		}
+	}
+	services = filteredServices
+
+	if len(services) != 1 {
+		return ErrNotOneSvcWithTargetPort
+	}
+
+	// Port-forward the Service with client-go.
+	var localPort int
+	failed := make(chan error)
+	for res, svc := range services {
+		namespace := svc.GetNamespace()
+		serviceName := svc.GetName()
+
+		var servicePort int
+		if req.Port == 0 {
+			// We will use the first port in Service if not specified.
+			servicePort = int(svc.Spec.Ports[0].Port)
+		} else {
+			servicePort = req.Port
+		}
+
+		cfg, err := clientcmd.BuildConfigFromFlags("", kubeops.GetKubeConfig(res))
+		if err != nil {
+			return err
+		}
+
+		clientset, err := kubernetes.NewForConfig(cfg)
+		if err != nil {
+			return err
+		}
+
+		// Get an available local port for forwarding.
+		minPort, maxPort := 1024, 65535
+
+		for {
+			localPort = rand.Intn(maxPort-minPort) + minPort
+
+			// Try to listen to the port.
+			addr := fmt.Sprintf(":%d", localPort)
+			listener, err := net.Listen("tcp", addr)
+			if err != nil {
+				continue
+			}
+			_ = listener.Close()
+
+			break
+		}
+
+		go func() {
+			err := ForwardPort(ctx, cfg, clientset, namespace, serviceName, servicePort, localPort)
+			failed <- err
+		}()
+	}
+
+	err := <-failed
+	return err
+}
+
+func ForwardPort(
+	ctx context.Context,
+	restConfig *rest.Config,
+	clientset *kubernetes.Clientset,
+	namespace, serviceName string,
+	servicePort, localPort int,
+) error {
+	svc, err := clientset.CoreV1().Services(namespace).Get(ctx, serviceName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	if svc == nil {
+		return fmt.Errorf("failed to find service: %s", serviceName)
+	}
+
+	labels := []string{}
+	for k, v := range svc.Spec.Selector {
+		labels = append(labels, strings.Join([]string{k, v}, "="))
+	}
+	label := strings.Join(labels, ",")
+
+	// Select the first pod to forward the target port.
+	pods, err := clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{
+		LabelSelector: label, Limit: 1,
+	})
+	if err != nil {
+		return err
+	}
+	if len(pods.Items) < 1 {
+		return fmt.Errorf("pods of the service '%s' not found", serviceName)
+	}
+	pod := pods.Items[0]
+
+	fmt.Printf("Forwarding localhost port to targetPort of pod '%s' selected by the service '%s' (%d:%d)\n",
+		pod.Name, serviceName, localPort, servicePort)
+
+	// Build a URL for SPDY connection for port-forwarding.
+	url := clientset.CoreV1().RESTClient().Post().
+		Resource("pods").Namespace(pod.Namespace).Name(pod.Name).
+		SubResource("portforward").URL()
+
+	transport, upgrader, err := spdy.RoundTripperFor(restConfig)
+	if err != nil {
+		return err
+	}
+
+	ports := []string{fmt.Sprintf("%d:%d", localPort, servicePort)}
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", url)
+
+	stop, ready := make(chan struct{}, 1), make(chan struct{})
+	out, errOut := os.Stdout, os.Stderr
+
+	fw, err := portforward.NewOnAddresses(dialer, []string{"localhost"}, ports, stop, ready, out, errOut)
+	if err != nil {
+		return err
+	}
+
+	return fw.ForwardPorts()
+}
