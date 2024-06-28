884ff58ab715ecccd9abe98211b4bb2a2c8b03d3
liu-hm19 <liuhaoming.lhm@antgroup.com>
2024-06-28 17:55:54 +0800

    feat: mask secret data in preview result (#1176)

--- pkg/util/diff/diff_test.go
+++ pkg/util/diff/diff_test.go
@@ -1,11 +1,13 @@
 package diff
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/gonvenience/ytbx"
 	"github.com/stretchr/testify/assert"
 
+	v1 "kusionstack.io/kusion/pkg/apis/api.kusion.io/v1"
 	"kusionstack.io/kusion/third_party/dyff"
 )
 
@@ -108,4 +110,202 @@ func TestToReport(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, len(report.Diffs), 2)
 	})
+}
+
+func TestMaskSensitiveData(t *testing.T) {
+	var nilResource *v1.Resource
+
+	testcases := []struct {
+		name                  string
+		oldData               interface{}
+		newData               interface{}
+		expectedMaskedOldData interface{}
+		expectedMaskedNewData interface{}
+	}{
+		{
+			name: "same old and new data",
+			oldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "dmFsdWUK",
+					},
+				},
+			},
+			newData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "dmFsdWUK",
+					},
+				},
+			},
+			expectedMaskedOldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "*******",
+					},
+				},
+			},
+			expectedMaskedNewData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "*******",
+					},
+				},
+			},
+		},
+		{
+			name: "different old and new data",
+			oldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "dmFsdWUK",
+					},
+				},
+			},
+			newData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "dmFsdWUtY2hhbmdlZAo=",
+					},
+				},
+			},
+			expectedMaskedOldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "***before***",
+					},
+				},
+			},
+			expectedMaskedNewData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "***after****",
+					},
+				},
+			},
+		},
+		{
+			name:    "empty old data",
+			oldData: nilResource,
+			newData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "dmFsdWUtY2hhbmdlZAo=",
+					},
+				},
+			},
+			expectedMaskedOldData: nilResource,
+			expectedMaskedNewData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"data": map[string]interface{}{
+						"key": "*******",
+					},
+				},
+			},
+		},
+		{
+			name: "not secret resource",
+			oldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Namespace",
+				},
+			},
+			newData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Namespace",
+				},
+			},
+			expectedMaskedOldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Namespace",
+				},
+			},
+			expectedMaskedNewData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Namespace",
+				},
+			},
+		},
+		{
+			name: "secrets with string data",
+			oldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"stringData": map[string]interface{}{
+						"key": "dmFsdWUK",
+					},
+				},
+			},
+			newData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"stringData": map[string]interface{}{
+						"key": "dmFsdWUtY2hhbmdlZAo=",
+					},
+				},
+			},
+			expectedMaskedOldData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"stringData": map[string]interface{}{
+						"key": "***before***",
+					},
+				},
+			},
+			expectedMaskedNewData: &v1.Resource{
+				Type: v1.Kubernetes,
+				Attributes: map[string]interface{}{
+					"kind": "Secret",
+					"stringData": map[string]interface{}{
+						"key": "***after****",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualMaskedOldData, actualMaskedNewData := maskSensitiveData(
+				tc.oldData, tc.newData,
+			)
+
+			if !reflect.DeepEqual(actualMaskedOldData, tc.expectedMaskedOldData) {
+				t.Errorf("masked old data does not match expected result, got: %v, wanted: %v",
+					actualMaskedOldData, tc.expectedMaskedOldData)
+			}
+
+			if !reflect.DeepEqual(actualMaskedNewData, tc.expectedMaskedNewData) {
+				t.Errorf("masked new data does not match expected result, get: %v, wanted: %v",
+					actualMaskedNewData, tc.expectedMaskedNewData)
+			}
+		})
+	}
 }
