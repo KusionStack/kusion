4318f92766bcf50758bc306b26a1bad672f655db
Yang Yang <yy449738@antgroup.com>
2024-09-23 14:04:35 +0800

    feat: add resource sub command (#1291)

---
+++ pkg/cmd/resource/resource.go
@@ -0,0 +1,27 @@
+package resource
+
+import (
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+	"k8s.io/kubectl/pkg/util/templates"
+	cmdutil "kusionstack.io/kusion/pkg/cmd/util"
+	"kusionstack.io/kusion/pkg/util/i18n"
+)
+
+var resLong = i18n.T(`
+		Commands for observing Kusion resources.
+		
+		These commands help you observe the information of Kusion resources within a project. `)
+
+// NewCmdRes returns an initialized Command instance for 'resource' sub command.
+func NewCmdRes(streams genericiooptions.IOStreams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                   "resource",
+		DisableFlagsInUseLine: true,
+		Short:                 "Observe Kusion resource information",
+		Long:                  templates.LongDesc(resLong),
+		Run:                   cmdutil.DefaultSubCommandRun(streams.ErrOut),
+	}
+
+	return cmd
+}
