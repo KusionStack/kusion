add69ba28d7db9ae42dc1fa9f394101e9234d15f
ffforest <forest10161016@gmail.com>
2024-11-13 16:19:52 +0800

    fix: able to parse run type and status in in any cases

--- pkg/domain/constant/run.go
+++ pkg/domain/constant/run.go
@@ -1,8 +1,6 @@
 package constant
 
-import (
-	"fmt"
-)
+import "strings"
 
 type (
 	RunType   string
@@ -25,37 +23,37 @@ const (
 // ParseRunType parses a string into a RunType.
 // If the string is not a valid RunType, it returns an error.
 func ParseRunType(s string) (RunType, error) {
-	switch s {
-	case string(RunTypeGenerate):
+	switch strings.ToLower(s) {
+	case strings.ToLower(string(RunTypeGenerate)):
 		return RunTypeGenerate, nil
-	case string(RunTypePreview):
+	case strings.ToLower(string(RunTypePreview)):
 		return RunTypePreview, nil
-	case string(RunTypeApply):
+	case strings.ToLower(string(RunTypeApply)):
 		return RunTypeApply, nil
-	case string(RunTypeDestroy):
+	case strings.ToLower(string(RunTypeDestroy)):
 		return RunTypeDestroy, nil
 	default:
-		return RunType(""), fmt.Errorf("invalid RunType: %q", s)
+		return RunType(""), nil
 	}
 }
 
 // ParseRunStatus parses a string into a RunStatus.
 // If the string is not a valid RunStatus, it returns an error.
 func ParseRunStatus(s string) (RunStatus, error) {
-	switch s {
-	case string(RunStatusScheduling):
+	switch strings.ToLower(s) {
+	case strings.ToLower(string(RunStatusScheduling)):
 		return RunStatusScheduling, nil
-	case string(RunStatusInProgress):
+	case strings.ToLower(string(RunStatusInProgress)):
 		return RunStatusInProgress, nil
-	case string(RunStatusFailed):
+	case strings.ToLower(string(RunStatusFailed)):
 		return RunStatusFailed, nil
-	case string(RunStatusSucceeded):
+	case strings.ToLower(string(RunStatusSucceeded)):
 		return RunStatusSucceeded, nil
-	case string(RunStatusCancelled):
+	case strings.ToLower(string(RunStatusCancelled)):
 		return RunStatusCancelled, nil
-	case string(RunStatusQueued):
+	case strings.ToLower(string(RunStatusQueued)):
 		return RunStatusQueued, nil
 	default:
-		return RunStatus(""), fmt.Errorf("invalid RunType: %q", s)
+		return RunStatus(""), nil
 	}
 }
