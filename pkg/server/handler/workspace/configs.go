35892853de97102cbdd643778635503287c387e6
liuhaoming <liuhaoming.lhm@antgroup.com>
2024-11-14 15:40:24 +0800

    fix: a lint error

--- pkg/server/handler/workspace/configs.go
+++ pkg/server/handler/workspace/configs.go
@@ -9,20 +9,20 @@ import (
 	"kusionstack.io/kusion/pkg/server/handler"
 )
 
-//	@Id				getWorkspaceConfigs
-//	@Summary		get workspace configurations
-//	@Description	Get configurations in the specified workspace
-//	@Tags			workspace
-//	@Accept			json
-//	@Produce		json
-//	@Param			id	path		int							true	"Workspace ID"
-//	@Success		200	{object}	request.WorkspaceConfigs	"Success"
-//	@Failure		400	{object}	error						"Bad Request"
-//	@Failure		401	{object}	error						"Unauthorized"
-//	@Failure		429	{object}	error						"Too Many Requests"
-//	@Failure		404	{object}	error						"Not Found"
-//	@Failure		500	{object}	error						"Internal Server Error"
-//	@Router			/api/v1/workspaces/{id}/configs [get]
+// @Id				getWorkspaceConfigs
+// @Summary		get workspace configurations
+// @Description	Get configurations in the specified workspace
+// @Tags			workspace
+// @Accept			json
+// @Produce		json
+// @Param			id	path		int							true	"Workspace ID"
+// @Success		200	{object}	request.WorkspaceConfigs	"Success"
+// @Failure		400	{object}	error						"Bad Request"
+// @Failure		401	{object}	error						"Unauthorized"
+// @Failure		429	{object}	error						"Too Many Requests"
+// @Failure		404	{object}	error						"Not Found"
+// @Failure		500	{object}	error						"Internal Server Error"
+// @Router			/api/v1/workspaces/{id}/configs [get]
 func (h *Handler) GetWorkspaceConfigs() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Getting stuff from the context.
@@ -38,20 +38,20 @@ func (h *Handler) GetWorkspaceConfigs() http.HandlerFunc {
 	}
 }
 
-//	@Id				validateWorkspaceConfigs
-//	@Summary		Validate workspace configurations
-//	@Description	Validate the configurations in the specified workspace
-//	@Tags			workspace
-//	@Accept			json
-//	@Produce		json
-//	@Param			workspace	body		request.WorkspaceConfigs	true	"Workspace configurations to be validated"
-//	@Success		200			{object}	request.WorkspaceConfigs	"Success"
-//	@Failure		400			{object}	error						"Bad Request"
-//	@Failure		401			{object}	error						"Unauthorized"
-//	@Failure		429			{object}	error						"Too Many Requests"
-//	@Failure		404			{object}	error						"Not Found"
-//	@Failure		500			{object}	error						"Internal Server Error"
-//	@Router			/api/v1/workspaces/configs/validate [post]
+// @Id				validateWorkspaceConfigs
+// @Summary		Validate workspace configurations
+// @Description	Validate the configurations in the specified workspace
+// @Tags			workspace
+// @Accept			json
+// @Produce		json
+// @Param			workspace	body		request.WorkspaceConfigs	true	"Workspace configurations to be validated"
+// @Success		200			{object}	request.WorkspaceConfigs	"Success"
+// @Failure		400			{object}	error						"Bad Request"
+// @Failure		401			{object}	error						"Unauthorized"
+// @Failure		429			{object}	error						"Too Many Requests"
+// @Failure		404			{object}	error						"Not Found"
+// @Failure		500			{object}	error						"Internal Server Error"
+// @Router			/api/v1/workspaces/configs/validate [post]
 func (h *Handler) ValidateWorkspaceConfigs() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Decode the request body into the payload.
@@ -66,21 +66,21 @@ func (h *Handler) ValidateWorkspaceConfigs() http.HandlerFunc {
 	}
 }
 
-//	@Id				updateWorkspaceConfigs
-//	@Summary		Update workspace configurations
-//	@Description	Update the configurations in the specified workspace
-//	@Tags			workspace
-//	@Accept			json
-//	@Produce		json
-//	@Param			id			path		int							true	"Workspace ID"
-//	@Param			workspace	body		request.WorkspaceConfigs	true	"Updated workspace configurations"
-//	@Success		200			{object}	request.WorkspaceConfigs	"Success"
-//	@Failure		400			{object}	error						"Bad Request"
-//	@Failure		401			{object}	error						"Unauthorized"
-//	@Failure		429			{object}	error						"Too Many Requests"
-//	@Failure		404			{object}	error						"Not Found"
-//	@Failure		500			{object}	error						"Internal Server Error"
-//	@Router			/api/v1/workspaces/{id}/configs [put]
+// @Id				updateWorkspaceConfigs
+// @Summary		Update workspace configurations
+// @Description	Update the configurations in the specified workspace
+// @Tags			workspace
+// @Accept			json
+// @Produce		json
+// @Param			id			path		int							true	"Workspace ID"
+// @Param			workspace	body		request.WorkspaceConfigs	true	"Updated workspace configurations"
+// @Success		200			{object}	request.WorkspaceConfigs	"Success"
+// @Failure		400			{object}	error						"Bad Request"
+// @Failure		401			{object}	error						"Unauthorized"
+// @Failure		429			{object}	error						"Too Many Requests"
+// @Failure		404			{object}	error						"Not Found"
+// @Failure		500			{object}	error						"Internal Server Error"
+// @Router			/api/v1/workspaces/{id}/configs [put]
 func (h *Handler) UpdateWorkspaceConfigs() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Getting stuff from context.
@@ -103,20 +103,20 @@ func (h *Handler) UpdateWorkspaceConfigs() http.HandlerFunc {
 	}
 }
 
-//	@Id				createWorkspaceModDeps
-//	@Summary		Create the module dependencies of the workspace
-//	@Description	Create the module dependencies in kcl.mod of the specified workspace
-//	@Tags			workspace
-//	@Accept			json
-//	@Produce		plain
-//	@Param			id											path		int		true	"Workspace ID"
-//	@Success		200											{object}	string	"Success"
-//	@Failure		400											{object}	error	"Bad Request"
-//	@Failure		401											{object}	error	"Unauthorized"
-//	@Failure		429											{object}	error	"Too Many Requests"
-//	@Failure		404											{object}	error	"Not Found"
-//	@Failure		500											{object}	error	"Internal Server Error"
-//	@Router			/api/v1/workspaces/{id}/configs/mod-deps 																[post]
+// @Id				createWorkspaceModDeps
+// @Summary		Create the module dependencies of the workspace
+// @Description	Create the module dependencies in kcl.mod of the specified workspace
+// @Tags			workspace
+// @Accept			json
+// @Produce		plain
+// @Param			id											path		int		true	"Workspace ID"
+// @Success		200											{object}	string	"Success"
+// @Failure		400											{object}	error	"Bad Request"
+// @Failure		401											{object}	error	"Unauthorized"
+// @Failure		429											{object}	error	"Too Many Requests"
+// @Failure		404											{object}	error	"Not Found"
+// @Failure		500											{object}	error	"Internal Server Error"
+// @Router			/api/v1/workspaces/{id}/configs/mod-deps 																[post]
 func (h *Handler) CreateWorkspaceModDeps() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Getting stuff from context.
