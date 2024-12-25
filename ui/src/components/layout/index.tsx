04ac52aebc23b0b5e38b9ba828bf1a6bfcd9b4fe
tianhai <wb-th358723@antgroup.com>
2024-12-25 15:15:13 +0800

    fix: login

--- ui/src/components/layout/index.tsx
+++ ui/src/components/layout/index.tsx
@@ -3,29 +3,12 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import NavLogo from './navLogo'
 import KusionMenu from './kusionMenu'
-import NavMenu from './navMenu'
 import NavRight from './navRight'
 
 import styles from './style.module.less'
-import { Card } from 'antd'
 
 
 const LayoutPage = () => {
-  const navigate = useNavigate()
-  const { pathname } = useLocation()
-  const { isLogin, isUnsafeMode } = useSelector((state: any) => state.globalSlice)
-
-
-  useEffect(() => {
-    if (
-      !isLogin &&
-      !isUnsafeMode &&
-      !['/login', '/', '/search']?.includes(pathname)
-    ) {
-      navigate('/login')
-    }
-  }, [isLogin, isUnsafeMode, navigate, pathname])
-
 
   return (
     <div className={styles.wrapper}>
