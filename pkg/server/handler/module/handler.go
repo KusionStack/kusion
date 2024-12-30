95de8b1936733ce98cdf1a83d7fb7017da2b0978
Xiaoming_Wuqi <liu-hm19@tsinghua.org.cn>
2024-12-30 17:00:37 +0800

    chore: update module handler query parameter
    Co-authored-by: Yang Yang <yy5478@hotmail.com>

--- pkg/server/handler/module/handler.go
+++ pkg/server/handler/module/handler.go
@@ -91,7 +91,7 @@ func (h *Handler) DeleteModule() http.HandlerFunc {
 // @Failure		429						{object}	error									"Too Many Requests"
 // @Failure		404						{object}	error									"Not Found"
 // @Failure		500						{object}	error									"Internal Server Error"
-// @Router			/api/v1/modules/{name} 																																																																																																																																																																	[put]
+// @Router			/api/v1/modules/{moduleName} 																																																																																																																																																																	[put]
 func (h *Handler) UpdateModule() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Getting stuff from context.
