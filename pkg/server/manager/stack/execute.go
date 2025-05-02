bd7c55ff024f227d5e4c87c32b417d57b64b07f1
7h3-3mp7y-m4n <115151332+7h3-3mp7y-m4n@users.noreply.github.com>
2025-05-02 18:34:39 +0530

    feat: handle empty stack in preview (#1423)
    * feat: handle empty stack in preview
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>

--- pkg/server/manager/stack/execute.go
+++ pkg/server/manager/stack/execute.go
@@ -198,11 +198,13 @@ func (m *StackManager) PreviewStack(ctx context.Context, params *StackRequestPar
 		return nil, err
 	}
 
-	// return immediately if no resource found in stack
-	// todo: if there is no resource, should still do diff job; for now, if output is json format, there is no hint
-	if sp == nil || len(sp.Resources) == 0 {
-		logutil.LogToAll(logger, runLogger, "Info", "No resource change found in this stack...")
-		return nil, nil
+	// Treat nil spec as empty and continue with diff
+	if sp == nil {
+		logutil.LogToAll(logger, runLogger, "Warn", "Generated spec is nil, treating as empty spec...")
+		sp = &apiv1.Spec{}
+	}
+	if len(sp.Resources) == 0 {
+		logutil.LogToAll(logger, runLogger, "Info", "No resources found in spec. Proceeding with full diff.")
 	}
 
 	// Preview
